7abdf1ba3aa7dc61ef3b9982157bf81a4393fdcf
LuisPerez <alejandroabril98@gmail.com>
2023-03-14 12:42:56 -0500

    Listar heroes, agregar heroes, buscar heroes

---
+++ Tour de heroes/Tour de heroes/IServices/IHeroService.cs
@@ -0,0 +1,12 @@
+﻿
+using Tour_de_heroes.Models;
+
+namespace Tour_de_heroes.IServices
+{
+    public interface IHeroService
+    {
+        public List<Hero> SearchHero(string nameHero);
+
+        public void addHero(Hero hero);
+    }
+}
---
+++ Tour de heroes/Tour de heroes/Services/HeroService.cs
@@ -0,0 +1,25 @@
+﻿
+using Tour_de_heroes.IServices;
+using Tour_de_heroes.Models;
+using System.Linq;
+
+namespace Tour_de_heroes.Services
+{
+    public class HeroService: IHeroService
+    {
+        public List<Hero> Heroes { get; } = new List<Hero>();
+
+
+        public List<Hero> SearchHero(string nameHero)
+        {
+            List<Hero> HeroFound = Heroes.Where(hero => hero.Nombre.Equals(nameHero)).ToList();
+            return HeroFound;
+        }
+
+        public void addHero(Hero hero)
+        {
+            Heroes.Add(hero);
+            Console.WriteLine($"DESDE EL SERVICIO ADDHERO: {Heroes}");
+        }
+    }
+}
